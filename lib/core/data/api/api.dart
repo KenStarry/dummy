c2fb833ab92052a4e5b45e3e3bad9da63691c1fe
KenStarry <starrycodes@gmail.com>
2024-01-01 01:49:32 +0300

    Added API.dart class

---
+++ lib/core/data/api/api.dart
@@ -0,0 +1,15 @@
+import "package:envied/envied.dart";
+
+part 'api.g.dart';
+
+//  dart run build_runner build -d
+@Envied(path: '.env')
+final class Api {
+  //  Url
+  @EnviedField(varName: 'SUPABASE_URL')
+  static const String url = _Api.url;
+
+  //  Anon Key
+  @EnviedField(varName: 'SUPABASE_ANON_KEY')
+  static const String anonKey = _Api.anonKey;
+}
