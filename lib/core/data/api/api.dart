6058881e0ae7f844446548d0fe8d7e37e76de62c
KenStarry <starrycodes@gmail.com>
2024-01-01 02:02:00 +0300

    Modified API class to accept default values

--- lib/core/data/api/api.dart
+++ lib/core/data/api/api.dart
@@ -4,12 +4,12 @@ part 'api.g.dart';
 
 //  dart run build_runner build -d
 @Envied(path: '.env')
-final class Api {
-  // //  Url
-  // @EnviedField(varName: 'SUPABASE_URL')
-  // static const String url = _Api.url;
-  //
-  // //  Anon Key
-  // @EnviedField(varName: 'SUPABASE_ANON_KEY')
-  // static const String anonKey = _Api.anonKey;
+abstract class Api {
+  //  Url
+  @EnviedField(varName: 'SUPABASE_URL', defaultValue: '')
+  static const String url = _Api.url;
+
+  //  Anon Key
+  @EnviedField(varName: 'SUPABASE_ANON_KEY', defaultValue: '')
+  static const String anonKey = _Api.anonKey;
 }
