2082f0dbee0424dec45f5fd376de0a22bf46bf91
KenStarry <starrycodes@gmail.com>
2024-01-01 01:53:56 +0300

    Generated API Key

--- lib/core/data/api/api.dart
+++ lib/core/data/api/api.dart
@@ -5,11 +5,11 @@ part 'api.g.dart';
 //  dart run build_runner build -d
 @Envied(path: '.env')
 final class Api {
-  //  Url
-  @EnviedField(varName: 'SUPABASE_URL')
-  static const String url = _Api.url;
-
-  //  Anon Key
-  @EnviedField(varName: 'SUPABASE_ANON_KEY')
-  static const String anonKey = _Api.anonKey;
+  // //  Url
+  // @EnviedField(varName: 'SUPABASE_URL')
+  // static const String url = _Api.url;
+  //
+  // //  Anon Key
+  // @EnviedField(varName: 'SUPABASE_ANON_KEY')
+  // static const String anonKey = _Api.anonKey;
 }
